3db55a61304b4c88403f22f6dd43f1784bbd59f1
Cristiano Altmann <cristiano.altmann@gamesys.co.uk>
2022-04-01 20:48:02 +0100

    Configure Spring

---
+++ src/main/java/com/crisaltmann/KafkaStreamStockApp.java
@@ -0,0 +1,13 @@
+package com.crisaltmann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamStockApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamStockApp.class);
+    }
+
+}
